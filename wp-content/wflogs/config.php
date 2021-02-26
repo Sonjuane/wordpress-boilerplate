aa33f1635fa632396514179241f45552cc634dbf
Sonny Jane <sonjuane@gmail.com>
2021-02-26 09:51:00 -0500

    Initial Commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614953552;s:7:"authKey";s:64:"-Z:B]SpKIe2<,OyK5E.zxNJn{<(w*T]*r(v`uUI2Tw^v:EYNN,_`/]qEAg>Y5%e3";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1137;}
